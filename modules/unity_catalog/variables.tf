8f06c3ce5a019914d88a6805173ab222a0428125
abdoudelson <larbi.abderahmane@gmail.com>
2026-02-04 23:22:05 +0100

    fix(deploy): deploying databricks ws + medallion archi containers adls

--- modules/unity_catalog/variables.tf
+++ modules/unity_catalog/variables.tf
@@ -1,3 +1,6 @@
 variable "name" {}
-variable "workspace_id" {}
+variable "workspace_id" {
+  type        = number
+  description = "Databricks workspace numeric ID for metastore assignment"
+}
 variable "storage_root" {}
--- root/main.tf
+++ root/main.tf
@@ -1,8 +1,16 @@
+
+# -----------------------------
+# Resource Group
+# -----------------------------
 module "resource_group" {
   source   = "../modules/resource_group"
   name     = "${local.prefix}-rg"
   location = local.location
 }
+
+# -----------------------------
+# Storage Account + Containers
+# -----------------------------
 
 module "storage" {
   source     = "../modules/storage"
@@ -11,6 +19,10 @@ module "storage" {
   location   = local.location
   containers = local.containers
 }
+
+# -----------------------------
+# Databricks Workspace
+# -----------------------------
 
 module "databricks_workspace" {
   source   = "../modules/databricks_workspace"
@@ -23,14 +35,10 @@ provider "databricks" {
   host = module.databricks_workspace.workspace_url
 }
 
-module "unity_catalog" {
-  source       = "../modules/unity_catalog"
-  name         = "${local.prefix}-metastore"
-  workspace_id = module.databricks_workspace.workspace_id
-  storage_root = "abfss://gold@${module.storage.account_name}.dfs.core.windows.net/"
-}
 
-
+# -----------------------------
+# Access Connector
+# -----------------------------
 module "access_connector" {
   source   = "../modules/access_connector"
   name     = "${local.prefix}-ac"
@@ -46,26 +54,13 @@ module "external_locations" {
   access_connector_id = module.access_connector.id
 }
 
-module "jobs" {
-  source       = "../modules/jobs"
-  environment  = var.environment
-  node_type_id = local.cluster_node_type[var.environment]
-  is_prod      = local.is_prod
-}
-
-module "dlt" {
-  source           = "../modules/dlt_pipeline"
-  name             = "bronze_pipeline"
-  catalog          = "main"   # Existing Unity Catalog metastore catalog
-  schema           = "bronze" # Schema inside the catalog
-  pipeline_storage = "dbfs:/pipelines/bronze"
-  continuous       = true
-}
-
-
+# -----------------------------
+# Role Assignment for Storage
+# -----------------------------
 resource "azurerm_role_assignment" "adls_uc" {
   scope                = module.storage.account_id
   role_definition_name = "Storage Blob Data Contributor"
   principal_id         = module.access_connector.principal_id
 }
+
 
