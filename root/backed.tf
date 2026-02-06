e34fe24594c1040800205dd0ba42cfbbb4d63029
abdoudelson <larbi.abderahmane@gmail.com>
2026-02-06 15:18:14 +0100

    feat(ci/cd): create deploy process based on branch and env

---
+++ root/backed.tf
@@ -0,0 +1,10 @@
+terraform {
+  backend "azurerm" {
+    # These values should be updated to match your infrastructure
+    # resource_group_name  = "rg-terraform-state"
+    # storage_account_name = "sttfstate"
+    # container_name       = "tfstate"
+    # key                  = "terraform.tfstate"
+    use_oidc = true
+  }
+}
